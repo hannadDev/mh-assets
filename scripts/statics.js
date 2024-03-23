1e5f72090be7157b2b4a1aabc66685d7c6ca5753
Mohannad Najjar <najjar.development@gmail.com>
2024-03-23 15:18:29 +0300

    v1.0.2
    - Added dragElement method
    - Added hd_statics.js

---
+++ scripts/statics.js
@@ -0,0 +1,16 @@
+let hd_statics = {};
+
+hd_statics.monthMap = new Map();
+
+monthMap.set("January", 1);
+monthMap.set("February", 2);
+monthMap.set("March", 3);
+monthMap.set("April", 4);
+monthMap.set("May", 5);
+monthMap.set("June", 6);
+monthMap.set("July", 7);
+monthMap.set("August", 8);
+monthMap.set("September", 9);
+monthMap.set("October", 10);
+monthMap.set("November", 11);
+monthMap.set("December", 12);
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,3 @@
-
 let hd_utils = {};
 
 hd_utils.addStyleElement = function (src) {
@@ -26,3 +25,40 @@ hd_utils.readJson = function (src, callback) {
   xmlhttp.open("GET", src, true);
   xmlhttp.send();
 };
+
+hd_utils.dragElement = function (elmnt, dragEl) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  dragEl.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
